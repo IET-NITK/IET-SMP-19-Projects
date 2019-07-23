ce9df1bb873c00abb5416c2122b32a43b6ec0fb9
skanda99 <skandaupadhyaya1999@gmail.com>
2019-07-23 07:10:33 +0530

    MATLAB

---
+++ MATLAB/Assignment 1/Vaibhav Sahai/ieta2.m
@@ -0,0 +1,7 @@
+x=pi/5
+a1=(cos(x/2))^2
+a2=(tan(x)+sin(x))/(2*tan(x))
+b1=cos(x/2)
+b2=sqrt((1+cos(x))/2)
+c1=tan(2*x)
+c2=(2*tan(x))/(1-((tan(x))^2))
---
+++ MATLAB/Assignment 2/VAibhav Sahai/ietww21.m
@@ -0,0 +1,8 @@
+name=input("enter name")
+rno=input('enter roll number')
+cs=input('enter sc cgpa')
+phy=input('enter phy cpga')
+chem=input('enter chem cgpa')
+am=input('enter am cgpa')
+civil=input('enter civil cgpa')
+cgpa=(4*cs+4*phy+3*chem+3*am+3*civil)/17
